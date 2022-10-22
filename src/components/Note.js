5d99ee6feb431d1372b26733834381689ba77e12
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-10-22 20:12:16 +0900

    Notes List component

---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import { MdDeleteForever } from 'react-icons/md';
+
+const Note = () => {
+    return <div className="note">
+        <span>Hello! This is outr first note! Hurray!</span>
+        <div className="note-footer">
+            <small>10/22/2022</small>
+            <MdDeleteForever className='delete-icon' size='1.3em' />
+        </div>
+    </div>;
+};
